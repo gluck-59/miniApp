6501fd6904bfbadbbfd15b06db51a5b1b42d1a6b
gluck <gluck59@gmail.com>
2025-03-21 17:48:25 +0500

    init

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get Your Possible Child Photo</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="main.js">
+    <link rel="icon" type="image/png" href="favicon.ico" />
+</head>
+<body>
+
+<div class="container">
+    <p class="clearfix">&nbsp;</p>
+    <center>
+        <img src="download.png" alt="" style="width: 10em">
+        <p class="clearfix">&nbsp;</p>
+        <h2 class="">Get Your Possible Child Photo</h2>
+    </center>
+    <p class="clearfix">&nbsp;</p>
+    <div class="row">
+        <div class="block col col-md-3 col-sm-12 col-xs-12">
+            111
+        </div>
+        <div class="block col col-md-3 col-sm-12 col-xs-12">
+            222
+        </div>
+        <div class="result block col col-md-3 col-sm-12 col-xs-12">
+            333
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
